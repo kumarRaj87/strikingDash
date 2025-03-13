080a260b12c02c8546351c6d3e04a9d961368ad5
KumarRaj21 <konnakumarraja097@gmail.com>
2025-03-13 12:40:43 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+
+      textShadow: {
+        sm: '0 1px 0 rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  plugins: [],
+}
